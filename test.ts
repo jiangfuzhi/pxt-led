1fbf17074020eac054ca9596f221ecfa198910db
jiangfuzhi <jfuzhi@163.com>
2020-01-10 18:22:37 +0800

    Updates.

--- test.ts
+++ test.ts
@@ -1,1 +1,18 @@
 // tests go here; this will not be compiled when this package is used as a library
+
+//% color="#AA278D" block = "LED"
+namespace led {
+
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
+    }
+}
